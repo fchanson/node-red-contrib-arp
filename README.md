163a5b2db538085901e48c03f245d62594ad42d5
Frédéric Chanson <chanson@mbp-de-chanson.boiscolombes.fr.ibm.com>
2017-04-20 16:08:58 +0200

    Create

---
+++ README.md
@@ -0,0 +1,35 @@
+# node-red-contrib-arp
+
+This node provides the content of the ARP table, **ONLY on Raspberry Pi**.<br>
+It returns the mapping of the network address (IP address) to a physical address (MAC address) of the devices which are connected to the same LAN that the Raspberry Pi.
+
+### Install
+
+From your node-red directory:
+
+    npm install node-red-contrib-arp
+    
+or
+    
+in the Node-red, Manage palette, Install node-red-contrib-arp
+
+
+
+### Usage
+
+
+This node provides the content of the ARP table, **ONLY on Raspberry Pi**.
+
+The output **msg.payload** is an array of objects containing : <br>
+
+- ip : the IP address of the device.
+- mac : the MAC address of the device.
+- iface : the network interface of the Raspberry Pi.
+
+It is possible to filter the results by MAC address :<br>
+
+- in the node configuration, by providing MAC address (separated by commas if multiple).
+- in the input **msg.payload.macs** message string, by providing MAC address (separated by commas if multiple).
+
+
+     
---
+++ arp/arp.js
@@ -0,0 +1,127 @@
+
+module.exports = function(RED) {
+	"use strict";
+	var os = require('os');
+	var exec = require('child_process').exec;	
+	
+	function PromiseFunc() {
+	    this.execCommand = function (cmd) {
+	        return new Promise(function(resolve, reject) {
+	           exec(cmd, function(error, stdout, stderr) {
+	             if (error) {
+	                reject(error);
+	                return;
+	            }
+	            resolve(stdout)
+	           });
+	       });
+	   }
+	};
+		
+	function ARP(n) {
+		RED.nodes.createNode(this, n);
+		this.name = n.name;
+		this.macs = n.macs;
+		var node = this;
+
+		node.on("input", function(msg) {
+			if (msg && msg.payload) {
+				node.macs = msg.payload.macs || node.macs;
+
+				var interfaces = os.networkInterfaces();
+
+				Object.keys(interfaces).forEach(function(inter) {
+
+					interfaces[inter].forEach(function(details) {
+						
+						if (!details.internal && details.family==='IPv4') {
+							
+							var ping = 'echo $(seq 254) | xargs -P255 -I% -d" " ping -W 1 -c 1 ';
+							ping += details.address.substr(0, details.address.lastIndexOf('.')+1) + '%';
+							ping += ' | grep -E "[0-1].*?:"';
+							
+							node.status({fill:"yellow",shape:"dot",text:"running"});
+							
+							exec(ping, function(error, stdout, stderr) {
+					             if (error) {
+					                return;
+					            }
+					        });
+							
+						}
+					});
+
+				});
+				
+				var execF = new PromiseFunc();
+				execF.execCommand('arp -n').then(function(res) {
+					node.status({fill:"green",shape:"dot",text:"finished"});
+					if(res && res.length>0) {
+						var json = [];
+						var lines = res.split('\n');
+						
+						for ( var i in lines) {
+							if(i==0) continue;
+							if(lines[i].indexOf('incomplete')<0) {
+								var cols = lines[i].split(' ');
+								var ip = '';
+								var mac = '';
+								var iface = '';
+								
+								for(var j in cols) {
+									if(cols[j].indexOf('.')>=0) {
+										ip = cols[j];
+										continue;
+									}
+									if(cols[j].indexOf(':')>=0) {
+										mac = cols[j];
+										continue;
+									}
+									if(j==cols.length-1) {
+										iface = cols[j];
+										continue;
+									}
+								}
+								
+								if(ip.length>0) {
+									var js = {};
+									js.ip = ip;
+									js.mac = mac;
+									js.iface = iface;
+									
+									if(node.macs) {
+										if(node.macs.toLowerCase().indexOf(mac) >=0) {
+											json.push(js);
+										}
+									} else {
+										json.push(js);
+									}
+
+									
+								}
+							}
+						}
+						
+
+						msg.payload = json;
+						
+						
+						return node.send(msg);
+					}
+
+				}).catch(function(err) {
+					node.status({fill:"red",shape:"dot",text:"error"});
+					node.error(err);
+					msg.payload.error = err;
+					return node.send(msg);
+				});
+				
+
+				
+			}
+		});
+
+	}
+	RED.nodes.registerType("arp", ARP);
+
+};
