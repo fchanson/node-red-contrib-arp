e0c62e09773d6ff5f9a3c9f0f93c67c676c12d56
Ristomatti Airo <ristomatti.airo@gmail.com>
2021-09-08 13:32:03 +0300

    Reformat for readability, remove extra whitespace

--- arp/arp.js
+++ arp/arp.js
@@ -1,9 +1,9 @@
-
 module.exports = function(RED) {
 	"use strict";
-	const os = require('os');
-	const exec = require('child_process').exec;	
 	
+	const os = require('os');
+	const exec = require('child_process').exec;
+
 	function PromiseFunc() {
 		this.execCommand = function(cmd) {
 			return new Promise(function(resolve, reject) {
@@ -17,7 +17,7 @@ module.exports = function(RED) {
 			});
 		}
 	};
-		
+
 	function ARP(n) {
 		RED.nodes.createNode(this, n);
 		this.name = n.name;
@@ -33,15 +33,15 @@ module.exports = function(RED) {
 				Object.keys(interfaces).forEach(function(inter) {
 
 					interfaces[inter].forEach(function(details) {
-						
-						if (!details.internal && details.family==='IPv4') {
-							
+
+						if (!details.internal && details.family === 'IPv4') {
+
 							let ping = 'echo $(seq 254) | xargs -P255 -I% -d" " ping -W 1 -c 1 ';
-							ping += details.address.substr(0, details.address.lastIndexOf('.')+1) + '%';
+							ping += details.address.substr(0, details.address.lastIndexOf('.') + 1) + '%';
 							ping += ' | grep -E "[0-1].*?:"';
-							
-							node.status({fill:"yellow",shape:"dot",text:"running"});
-							
+
+							node.status({ fill: "yellow", shape: "dot", text: "running" });
+
 							exec(ping, function(error, stdout, stderr) {
 								if (error) {
 									return;
@@ -51,72 +51,68 @@ module.exports = function(RED) {
 					});
 
 				});
-				
+
 				const execF = new PromiseFunc();
 				execF.execCommand('arp -n').then(function(res) {
-					node.status({fill:"green",shape:"dot",text:"finished"});
-					if(res && res.length>0) {
+					node.status({ fill: "green", shape: "dot", text: "finished" });
+					if (res && res.length > 0) {
 						const json = [];
 						const lines = res.split('\n');
-						
-						for ( const i in lines) {
-							if(i==0) continue;
-							if(lines[i].indexOf('incomplete')<0) {
+
+						for (const i in lines) {
+							if (i == 0) continue;
+							if (lines[i].indexOf('incomplete') < 0) {
 								const cols = lines[i].split(' ');
 								let ip = '';
 								let mac = '';
 								let iface = '';
-								
-								for(const j in cols) {
-									if(cols[j].indexOf('.')>=0) {
+
+								for (const j in cols) {
+									if (cols[j].indexOf('.') >= 0) {
 										ip = cols[j];
 										continue;
 									}
-									if(cols[j].indexOf(':')>=0) {
+									if (cols[j].indexOf(':') >= 0) {
 										mac = cols[j];
 										continue;
 									}
-									if(j==cols.length-1) {
+									if (j == cols.length - 1) {
 										iface = cols[j];
 										continue;
 									}
 								}
-								
-								if(ip.length>0) {
+
+								if (ip.length > 0) {
 									const js = {};
 									js.ip = ip;
 									js.mac = mac;
 									js.iface = iface;
-									
-									if(node.macs) {
-										if(node.macs.toLowerCase().indexOf(mac) >=0) {
+
+									if (node.macs) {
+										if (node.macs.toLowerCase().indexOf(mac) >= 0) {
 											json.push(js);
 										}
 									} else {
 										json.push(js);
 									}
 
-									
+
 								}
 							}
 						}
-						
 
 						msg.payload = json;
-						
-						
+
 						return node.send(msg);
 					}
 
 				}).catch(function(err) {
-					node.status({fill:"red",shape:"dot",text:"error"});
+					node.status({ fill: "red", shape: "dot", text: "error" });
 					node.error(err);
 					msg.payload.error = err;
 					return node.send(msg);
 				});
-				
 
-				
 			}
 		});
 
